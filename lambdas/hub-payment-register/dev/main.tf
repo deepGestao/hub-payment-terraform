e7023aa56555f2b77712e63e8321544a61b4fbfc
k.tavares.martins <k.tavares.martins@accenture.com>
2024-09-19 14:15:29 -0300

    add method_response

--- lambdas/hub-payment-register/dev/main.tf
+++ lambdas/hub-payment-register/dev/main.tf
@@ -137,3 +137,10 @@ resource "aws_lambda_permission" "apigw_lambda" {
 
   source_arn = "arn:aws:execute-api:${var.aws_region}:${var.aws_account}:${data.aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.resource.path}"
 }
+
+resource "aws_api_gateway_method_response" "response_200" {
+  rest_api_id = data.aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.resource.id
+  http_method = aws_api_gateway_method.method.http_method
+  status_code = "200"
+}
